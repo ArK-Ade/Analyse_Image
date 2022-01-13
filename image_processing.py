114727003bfe47a2ff0e4b02e82b13fd82ddc8e7
Anthony ADELAIDE <anthony.adelaide@orange.fr>
2022-01-13 17:00:49 +0100

    Correction

--- image_processing.py
+++ image_processing.py
@@ -23,6 +23,7 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
 # 2D convolution (averaging filter) (partie 2 question 1)
+
 kernel_5 = np.ones((5, 5), np.float32) / 25
 dst5 = cv2.filter2D(img, -1, kernel_5)
 cv2.imshow("kernel=5 blur", dst5)
@@ -36,26 +37,24 @@ dst15 = cv2.filter2D(img, -1, kernel_15)
 cv2.imshow("kernel=15 blur", dst15)
 
 # Gaussian blurring + median blurring (partie 2 question 2)
-"""
 gaussianBlur = cv2.GaussianBlur(img,(5,5),0)
 cv2.imshow("gaussian blur (5,5)", gaussianBlur)
 
 medianBlur = cv2.medianBlur(img,5)
 cv2.imshow("median blur (5)", medianBlur)
-"""
 
 # Partie 2 question 3 : application des filtres de sobel
-"""
-img_zebre = cv2.imread('zebre.jpg')
+
+img_zebre = cv2.imread('Images2020/zebre.jpg')
 img_zebre = cv2.cvtColor(img_zebre, cv2.COLOR_BGR2GRAY)
 sobelx_zebre = cv2.Sobel(img_zebre, cv2.CV_64F, 1, 0, ksize=5)
 cv2.imshow("filtre sobel vertical (5)", sobelx_zebre)
 
-img_suzan = cv2.imread('suzan.jpg')
+img_suzan = cv2.imread('Images2020/suzan.jpg')
 img_suzan = cv2.cvtColor(img_suzan, cv2.COLOR_BGR2GRAY)
 sobely_suzan = cv2.Sobel(img_suzan, cv2.CV_64F, 0, 1, ksize=5)
 cv2.imshow("filtre sobel horizontal (5)", sobely_suzan)
-"""
+
 
 # waits for user to press any key
 # (this is necessary to avoid Python kernel form crashing)
